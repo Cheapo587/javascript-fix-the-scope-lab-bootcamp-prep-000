73051fbf1e8f27becbdbbc21a672664a68625038
Cheapo587 <ac.shultz587@gmail.com>
2017-10-05 22:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,19 +35,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = funkyFunction.function()
-function
 
-
-/*
-
-function funkyFunction(){
-
-  return function inner(){
-
-    return "FUNKY!"
-  
-  }
-
-}
-
-*/
